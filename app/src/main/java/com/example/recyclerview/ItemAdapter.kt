da6c71f0fb8c644fb1fc99501c5406947290a616
Sydney Swofford <Sydney.Swofford@omron.com>
2023-03-06 11:13:17 -0800

    Changed the color scheeme of the app to be black like my heart

--- app/src/main/java/com/example/recyclerview/ItemAdapter.kt
+++ app/src/main/java/com/example/recyclerview/ItemAdapter.kt
@@ -9,16 +9,11 @@ import androidx.recyclerview.widget.RecyclerView
 class ItemAdapter(private val items: List<Item>) : RecyclerView.Adapter<ItemAdapter.ViewHolder>() {
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
-        // TODO: Create member variables for any view that will be set
-        // as you render a row.
         val itemtitle: TextView
         val linktitle: TextView
         val pricetitle: TextView
 
-        // We also create a constructor that accepts the entire item row
-        // and does the view lookups to find each sub-view
         init {
-            // TODO: Store each of the layout's views into
             // the public final member variables created above
             itemtitle=itemView.findViewById(R.id.itemTitle)
             linktitle=itemView.findViewById(R.id.linkTitle)
@@ -27,7 +22,6 @@ class ItemAdapter(private val items: List<Item>) : RecyclerView.Adapter<ItemAdap
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        //TODO("Not yet implemented")
         val context = parent.context
         val inflater = LayoutInflater.from(context)
         val wishlistView = inflater.inflate(R.layout.wishlist_item,parent,false)
@@ -35,12 +29,10 @@ class ItemAdapter(private val items: List<Item>) : RecyclerView.Adapter<ItemAdap
     }
 
     override fun getItemCount(): Int {
-        //TODO("Not yet implemented")
         return items.size
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        //TODO("Not yet implemented")
         val item = items.get(position)
         holder.itemtitle.text=item.itemname
         holder.linktitle.text=item.link
